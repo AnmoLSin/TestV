f9428a3a0d6496f93827643128e864c1d66baf9c
Anmol Singh <anisaggu8@gmail.com>
2022-11-09 16:29:49 +0530

    Query 2 Solution

--- src/test/java/tests/QuerySolution.java
+++ src/test/java/tests/QuerySolution.java
@@ -20,7 +20,24 @@ public class QuerySolution {
 				FP++;	
 			}
 		}
-		
+
 		Assert.assertEquals(FP, 4);
+	}
+	
+	@Test
+	public void Test_2() {
+		Response response = RestAssured.get("https://gist.githubusercontent.com/kumarpani/1e759f27ae302be92ad51ec09955e765/raw/184cef7125e6ef5a774e60de31479bb9b2884cb5/TeamRCB.json");
+		String resp = response.getBody().asString();
+		JsonPath json = new JsonPath(resp); 
+		int count = json.getInt("player.size()"), FP = 0;
+		
+		for(int i = 0; i < count; i++) {
+			if(!json.getString("player["+i+"].role").equals("Wicket-keeper")) {
+				FP++;
+				break;
+			}
+		}
+
+		Assert.assertEquals(FP, 1);
 	}
 }
